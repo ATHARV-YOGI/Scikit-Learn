b055e9de75d83604844930a74231ddf501b01c74
Atharv Yogi <atharvyogi123@gmail.com>
2025-09-05 12:58:59 +0530

    commit

---
+++ Linear_Regression/marks_predict.py
@@ -0,0 +1,13 @@
+from sklearn.linear_model import LinearRegression 
+x = [[1],[2],[3],[4],[5]]
+y = [40,50,65,75,90]
+
+model = LinearRegression()
+
+model.fit(x,y)
+
+hours = float(input("enter how many hours you studied = "))
+
+predicted_marks = model.predict([[hours]])
+
+print(f"based on your hours {hours} you may score around {predicted_marks}")
